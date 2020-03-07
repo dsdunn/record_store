9b642518d3d879ae9eae39ae05b8957984f12bf5
dsdunn <dsdunnmusic@gmail.com>
2020-03-07 10:13:27 -0700

    Setup Boilerplate

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('*', (req,res) => {
+  res.send('you\'ve hit the api @ port ' + (process.env.PORT || 8080))
+});
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log('app running on port ' + port);
+})
